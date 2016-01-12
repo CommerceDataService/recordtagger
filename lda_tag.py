beff52cd7bcde6243453d66a04ac9af6830ad19a
rebeccabilbro <bilbro@gmail.com>
2016-01-12 15:25:55 -0500

    cleaned up data ingestion for LDA

--- lda_tag.py
+++ lda_tag.py
@@ -1,102 +1,109 @@
-#########################################################################################
-# Topic extraction w/ Latent Dirichlet Allocation
-#########################################################################################
+# lda_tag.py
+#
+#
+# Title:        Topic extraction using Latent Dirichlet Allocation
+# Author:       Rebecca Bilbro
+# Date:         1/9/16
+# Organization: Commerce Data Service, U.S. Department of Commerce
+
+
 """
-In LDA, time complexity is proportional to (n_samples * iterations).
+Notes: In LDA, time complexity is proportional to (n_samples * iterations).
 """
 
+#####################################################################
+# Imports
+#####################################################################
 from __future__ import print_function  # Not necessary for Python 3
 from time import time
 
 import re
 import json
+import requests
+from sklearn.feature_extraction import text
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
-n_features = 2000
-n_topics = 200
+#####################################################################
+# Global Variables
+#####################################################################
+n_features = 1000
+n_topics = 20
 n_top_words = 30
 
+domain_stops = ["department","commerce","doc","noaa","national", "data", \
+                "centers", "united", "states", "administration"]
+stopwords = text.ENGLISH_STOP_WORDS.union(domain_stops)
 
-def print_top_words(model, feature_names, n_top_words):
+
+#####################################################################
+# Helper Functions
+#####################################################################
+def print_clusters(model, feature_names, n_top_words):
+    """
+    Takes the model, the names of the features, and the
+    requested number of top words for each cluster, and
+    prints out each cluster.
+    """
     for topic_idx, topic in enumerate(model.components_):
         print("Topic #%d:" % (topic_idx+1))
         print(" ".join([feature_names[i]
                         for i in topic.argsort()[:-n_top_words - 1:-1]]))
     print()
 
+def load_data(URL):
+    """
+    Loads the data from URL and returns data in JSON format.
+    """
+    r = requests.get(URL)
+    data = r.json()
+    return data
 
-print("Loading dataset...")
-t0 = time()
-
-r = requests.get("https://data.noaa.gov/data.json")
-noaa = r.json()
-data_samples = []
-for entry in noaa:
-    title = ' '.join(filter(lambda x: x.isalpha(), entry[u'title'].split()))
-    description = ' '.join(filter(lambda x: x.isalpha(), entry[u'description'].split()))
-    data_samples.append(title+" "+description+' '.join(filter(lambda x: x.isalpha(), entry[u'keyword']))+"\n")
-
-print("done in %0.3fs." % (time() - t0))
-
-stopwords = ["a", "about", "above", "above", "across", "after", "afterwards", "again", \
-             "against", "all", "almost", "alone", "along", "already", "also","although",\
-             "always","am", "among", "amongst", "amoungst", "amount",  "an", "and", "another", \
-             "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  \
-             "at", "back","be","became", "because","become","becomes", "becoming", "been", \
-             "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", \
-             "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", \
-             "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", \
-             "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", \
-             "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", \
-             "everywhere", "except", "few", "fifteen", "fifty", "fill", "find", "fire", "first", \
-             "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", \
-             "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", \
-             "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", \
-             "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", \
-             "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", \
-             "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", \
-             "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", \
-             "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", \
-             "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", \
-             "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", \
-             "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", \
-             "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", \
-             "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", \
-             "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", \
-             "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", \
-             "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", \
-             "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", \
-             "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", \
-             "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", \
-             "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", \
-             "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon",\
-             "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", \
-             "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "", \
-             "yours", "yourself", "yourselves", "the", "0", "1", "2", "3", "4", "5", "6", "7", "8", \
-             "9","10","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010",\
-             "2011","2012","2013","2014","2015","department","commerce","doc","noaa","national",\
-             "data", "centers", "united", "states", "administration"]
+def wrangle_data(json_data):
+    """
+    Takes JSON input data, extracts and joins the content from the relevant
+    fields for each record (keyword, title, description) and returns a list.
+    """
+    data_samples = []
+    for entry in json_data:
+        title = ' '.join(filter(lambda x: x.isalpha(), entry[u'title'].split()))
+        description = ' '.join(filter(lambda x: x.isalpha(), entry[u'description'].split()))
+        data_samples.append(title+" "+description+' '.join(filter(lambda x: x.isalpha(), entry[u'keyword']))+"\n")
+    return data_samples
 
 
-# Use tf (raw term count) features for LDA.
-print("Extracting tf features for LDA...")
-tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=n_features,
-                                stop_words=stopwords)
-t0 = time()
-tf = tf_vectorizer.fit_transform(data_samples)
-print("done in %0.3fs." % (time() - t0))
+if __name__ == '__main__':
+    # Start the clock
+    t0 = time()
 
-# Fit the LDA model
-print("Fitting LDA models with tf features, n_samples=%d and n_features=%d..."
-      % (len(data_samples), n_features))
-lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5,
-                                learning_method='online', learning_offset=50.,
-                                random_state=0)
-t0 = time()
-lda.fit(tf)
-print("done in %0.3fs." % (time() - t0))
+    # Load the data
+    print("Loading dataset...")
+    noaa = load_data("https://data.noaa.gov/data.json")
+    noaa_samples = wrangle_data(noaa)
+    print("done in %0.3fs." % (time() - t0))
 
-print("\nTopics in LDA model:")
-tf_feature_names = tf_vectorizer.get_feature_names()
-print_top_words(lda, tf_feature_names, n_top_words)
+    # Restart the clock
+    t0 = time()
+
+    # Extract raw term counts to compute term frequency.
+    print("Extracting tf features for LDA...")
+    tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=n_features,
+                                    stop_words=stopwords)
+    tf = tf_vectorizer.fit_transform(noaa_samples)
+    print("done in %0.3fs." % (time() - t0))
+
+    # Restart the clock
+    t0 = time()
+
+    # Fit the LDA model
+    print("Fitting LDA models with tf features, n_samples=%d and n_features=%d..."
+        % (len(noaa_samples), n_features))
+    lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5, learning_offset=50.,
+                                    random_state=0)
+    lda.fit(tf)
+    print("done in %0.3fs." % (time() - t0))
+
+    # Print out the clusters
+    print("\nTopics in LDA model:")
+    tf_feature_names = tf_vectorizer.get_feature_names()
+    print_clusters(lda, tf_feature_names, n_top_words)
