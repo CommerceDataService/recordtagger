4241757e30c76632cb65473601c2704a66276caa
rebeccabilbro <bilbro@gmail.com>
2016-01-21 09:53:56 -0500

    added method to save clusters to disk

--- lda_tag.py
+++ lda_tag.py
@@ -18,6 +18,7 @@ from __future__ import print_function  # Not necessary for Python 3
 from time import time
 
 import re
+import csv
 import json
 import requests
 from sklearn.feature_extraction import text
@@ -27,7 +28,7 @@ from sklearn.decomposition import LatentDirichletAllocation
 #####################################################################
 # Global Variables
 #####################################################################
-n_features = 20000
+n_features = 200000
 n_topics = 200
 n_top_words = 30
 
@@ -50,6 +51,21 @@ def print_clusters(model, feature_names, n_top_words):
         print(" ".join([feature_names[i]
                         for i in topic.argsort()[:-n_top_words - 1:-1]]))
     print()
+
+def save_clusters(model, feature_names, n_top_words):
+    """
+    Takes the model, the names of the features, and the
+    requested number of top words for each cluster, and
+    returns each cluster.
+    """
+    for topic_idx, topic in enumerate(model.components_):
+        t = "Topic #%d:" % (topic_idx+1)
+        topwords = []
+        x = topic.argsort()
+        y = x[:-n_top_words - 1:-1]
+        for i in y:
+            topwords.append(feature_names[i].encode('utf-8'))
+        yield t,topwords
 
 def load_data(URL):
     """
@@ -74,38 +90,44 @@ def wrangle_data(json_data):
 
 
 if __name__ == '__main__':
-    # Start the clock
-    t0 = time()
-    print("Benchmark at %d features" % n_features)
+    with open('lda_clusters.csv', 'wb') as f1:
+        writer = csv.writer(f1)
+        writer.writerow(["cluster","top words"])
 
-    # Load the data
-    print("Loading dataset...")
-    noaa = load_data("https://data.noaa.gov/data.json")
-    noaa_samples = wrangle_data(noaa)
-    print("done in %0.3fs." % (time() - t0))
+        # Start the clock
+        t0 = time()
 
-    # Restart the clock
-    t0 = time()
+        # Load the data
+        print("Loading dataset...")
+        noaa = load_data("https://data.noaa.gov/data.json")
+        noaa_samples = wrangle_data(noaa)
+        print("done in %0.3fs." % (time() - t0))
 
-    # Extract raw term counts to compute term frequency.
-    print("Extracting term frequency features for LDA...")
-    tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, #ngram_range=(1,2),
-                                    stop_words=stopwords)
-    tf = tf_vectorizer.fit_transform(noaa_samples)
-    print("done in %0.3fs." % (time() - t0))
+        # Restart the clock
+        t0 = time()
 
-    # Restart the clock
-    t0 = time()
+        # Extract raw term counts to compute term frequency.
+        print("Extracting term frequency features for LDA...")
+        tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, ngram_range=(1,2),
+                                        stop_words=stopwords)
+        tf = tf_vectorizer.fit_transform(noaa_samples)
+        print("done in %0.3fs." % (time() - t0))
 
-    # Fit the LDA model
-    print("Fitting LDA model with term frequency features, n_samples=%d and n_features=%d..."
-        % (len(noaa_samples), n_features))
-    lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5, learning_offset=50.,
-                                    random_state=0)
-    lda.fit(tf)
-    print("done in %0.3fs." % (time() - t0))
+        # Restart the clock
+        t0 = time()
 
-    # Print out the clusters
-    print("\nTopics in LDA model:")
-    tf_feature_names = tf_vectorizer.get_feature_names()
-    print_clusters(lda, tf_feature_names, n_top_words)
+        # Fit the LDA model
+        print("Fitting LDA model with term frequency features, n_samples=%d and n_features=%d..."
+            % (len(noaa_samples), n_features))
+        lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5, learning_offset=50.,
+                                        random_state=0)
+        lda.fit(tf)
+        print("done in %0.3fs." % (time() - t0))
+
+        # Print out the clusters
+        tf_feature_names = tf_vectorizer.get_feature_names()
+        print_clusters(lda, tfidf_feature_names, n_top_words)
+
+        # Save the clusters
+        for x in save_clusters(lda, tf_feature_names, n_top_words):
+            writer.writerow([str(x[0]),str(x[1])])
